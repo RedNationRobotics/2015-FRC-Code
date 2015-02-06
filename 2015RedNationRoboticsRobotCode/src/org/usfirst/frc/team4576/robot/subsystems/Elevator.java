1901b48a18bfef17ff730b27ad2329dc6546b5e4
backspace119 <backspace119rd@gmail.com>
2015-02-06 18:48:56 -0500

    added more stuff

--- 2015RedNationRoboticsRobotCode/src/org/usfirst/frc/team4576/robot/subsystems/Elevator.java
+++ 2015RedNationRoboticsRobotCode/src/org/usfirst/frc/team4576/robot/subsystems/Elevator.java
@@ -2,11 +2,17 @@ package org.usfirst.frc.team4576.robot.subsystems;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Elevator extends Subsystem{
 
+	public Elevator()
+	{
+		zero();
+	}
 	CANTalon elevTalon = new CANTalon(5);
+	Encoder e1 = new Encoder(4,5);
 	DigitalInput d6 = new DigitalInput(6);
 	DigitalInput d7 = new DigitalInput(7);
 	@Override
@@ -19,6 +25,7 @@ public class Elevator extends Subsystem{
 		while(!d6.get()){
 		elevTalon.set(-0.5);
 		}
+		e1.reset();
 	}
 	
 	public void up()
