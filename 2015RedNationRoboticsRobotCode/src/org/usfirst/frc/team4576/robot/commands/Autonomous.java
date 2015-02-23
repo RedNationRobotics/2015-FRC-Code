9e1207c7ecea0955ccacb5f4aa157212c2b41aca
backspace119 <backspace119rd@gmail.com>
2015-02-23 13:24:09 -0500

    Did not include new Autonomous file in last commit

---
+++ 2015RedNationRoboticsRobotCode/src/org/usfirst/frc/team4576/robot/commands/Autonomous.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc.team4576.robot.commands;
+
+import org.usfirst.frc.team4576.robot.Robot;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Autonomous extends Command{
+
+	@Override
+	protected void initialize() {
+		requires(Robot.chassis);
+		requires(Robot.pneumatics);
+		requires(Robot.elevator);
+		Robot.elevator.zero();
+		Robot.chassis.initAuto();
+		Robot.chassis.setLeftRight(1, 1);
+		Timer.delay(2);
+		Robot.pneumatics.setArms(true);
+		Timer.delay(1);
+		Robot.elevator.up();
+		Timer.delay(2);
+		Robot.chassis.disable();
+		Timer.delay(.25);
+		Robot.elevator.down();
+		Timer.delay(1);
+		Robot.pneumatics.setArms(false);
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
